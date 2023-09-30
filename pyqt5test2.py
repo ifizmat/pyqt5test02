9c034c925098f6b8bd60b1b60a843d45ad360e16
ifizmat <ifizmat@gmail.com>
2023-09-30 18:38:11 +0400

    QtDesigner QDialog form with test_label.

---
+++ pyqt5test2.py
@@ -0,0 +1,40 @@
+﻿from PyQt5 import uic, QtWidgets
+
+Form, _ = uic.loadUiType("mainwindow.ui")
+
+class Ui(QtWidgets.QDialog, Form):
+    def __init__(self):
+        super(Ui, self).__init__()
+        self.setupUi(self)
+        self.label_test.setText("UI started... OK!")
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    main_win = Ui()
+    main_win.show()
+    sys.exit(app.exec_())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
